09df3444ea6bc4be2fca29c262ec2620c7ae6424
anxxity <reminmohammed123@gmail.com>
2025-01-01 01:04:44 +0530

    radio

---
+++ client.lua
@@ -0,0 +1,20 @@
+
+ESX = exports["es_extended"]:getSharedObject()
+local PlayerData                = {}
+
+Citizen.CreateThread(function()
+	Citizen.Wait(5000)
+	PlayerData = ESX.GetPlayerData()
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+            if IsPedInAnyVehicle(PlayerPedId()) then
+                SetUserRadioControlEnabled(false)
+                if GetPlayerRadioStationName() ~= nil then
+                SetVehRadioStation(GetVehiclePedIsIn(PlayerPedId()),"OFF")
+                end
+        end
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game "gta5"
+
+author "Remin Moahmmed"
+description 'diables radio on fivem for esx'
+
+
+client_script {
+    'client.lua'
+}
+
+shared_script '@es_extended/imports.lua'
